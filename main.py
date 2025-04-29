89e41ed7ef834c802bb7a53fc62e72260571f3cd
Marco Islas <marco3islas@gmail.com>
2025-04-29 10:14:19 -0600

    se modificaron los archivos para ser compatibles con railway

--- main.py
+++ main.py
@@ -2,13 +2,18 @@ import os
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 CORS(app)
 
+load_dotenv()
 # Configuración para PostgreSQL
-app.config['SECRET_KEY'] = "f02834cddc43ff08ec46bcf040faba74ddd79fea8fdfe13db8464265fa909e30"
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://tienda_tenis_db_user:FiOtpW1hu773lPHU4FxZkhIMYkJBcBWR@dpg-cv4usvfnoe9s73er3010-a.oregon-postgres.render.com/tienda_tenis_db"
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+conexion = app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('PASSWORD')}@{os.getenv('HOST')}:{os.getenv('PORT')}/{os.getenv('DATABASE')}"
+print("esta es la conexion", conexion)
+
+
 
 db = SQLAlchemy(app)
 
